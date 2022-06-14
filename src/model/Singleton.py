f270a24aeb0bd76b1a72914e3c28f8be01005033
Pedro Binotto <pedrosantibinotto@gmail.com>
2022-06-14 01:10:01 -0300

    Abstract e Singleton por herança direta

---
+++ src/model/Singleton.py
@@ -0,0 +1,7 @@
+from model.Abstract import Abstract
+
+class Singleton(Abstract):
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Singleton, cls).__new__(cls)
+        return cls.instance
--- src/run.py
+++ src/run.py
@@ -1,5 +1,4 @@
 from model.ContaCorrente import ContaCorrente
-from model.Banco import Banco
 
 def main():
     conta = ContaCorrente(cpf_titular='119.143.139-84')
